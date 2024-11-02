faf7aceace6e94816fe0e6c219426698e4309398
operator_IO <vova.chigorin@yandex.ru>
2024-11-02 21:13:44 +0300

    Добавьте файлы проекта.

---
+++ LessonStore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LessonStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPriceInGold = 15;
+
+            Console.WriteLine("Введите ваше количество золота...");
+            int numGold = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Приобрести кристалл по цене: {crystalPriceInGold}/шт");
+            int numCrystal = int.Parse(Console.ReadLine());
+
+            СurrencyExchanger(numGold, numCrystal, crystalPriceInGold);
+        }
+        private static void СurrencyExchanger(int numGold, int numCrystal, int crystalPriceInGold)
+        {
+            double totalNumGold = numGold - (numCrystal * crystalPriceInGold);
+
+            Console.WriteLine($"У вас осталось: {totalNumGold} золота\nКристаллов: {numCrystal}шт.");
+        }
+    }
+}
